196ba0625e97ca4204fed014c4847f739fa503f7
ildusv <ildus.rafkatovich@gmail.com>
2026-02-08 23:51:54 +0800

    rp2040 files

---
+++ Software/Keypad/RP2040/freertos_test_project/main.cpp
@@ -0,0 +1,42 @@
+#include "FreeRTOS.h"
+#include "task.h"
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+
+#ifdef __cplusplus
+extern "C" {
+void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName );
+void vApplicationTickHook( void );
+void vApplicationMallocFailedHook( void );
+}
+#endif
+
+void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName ) {}
+void vApplicationTickHook( void ) {}
+void vApplicationMallocFailedHook( void ) {}
+
+constexpr int LED_PIN = 25;
+
+void vBlink(void* unused_arg) {
+    stdio_init_all();
+    for (;;) {
+
+        gpio_put(LED_PIN, 1);
+        vTaskDelay(1000);
+        gpio_put(LED_PIN, 0);
+        //puts("Hello FreeRTOS\n");
+        printf("Hello FreeRTOS\n");
+        vTaskDelay(1000);
+    }
+}
+
+int main() {
+    gpio_init(LED_PIN);
+
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    xTaskCreate(vBlink, "Blink", 128, NULL, 1, NULL);
+
+    vTaskStartScheduler();
+}
