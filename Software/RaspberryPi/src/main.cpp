3b8dc180c763ffab7bbadf18fa0c8db58cf26c78
ildusv <ildus.rafkatovich@gmail.com>
2026-02-14 18:47:16 +0800

    added jlx25664 support via i2c

--- Software/RaspberryPi/src/main.cpp
+++ Software/RaspberryPi/src/main.cpp
@@ -1,6 +1,9 @@
 #include "KeypadHandler.h"
 #include "MqttMgr.h"
+#include "linux-i2c.h"
+#include <u8g2.h>
 #include <iostream>
+#include <stdio.h>
 
 void on_keypad_msg_received(unsigned char* payload, size_t size) {
     std::cout << "Payload received: ";
@@ -15,13 +18,31 @@ void on_mqtt_msg_received(const std::string& topic, const std::string& payload) 
               << " with payload: " << payload << std::endl;
 }
 
+u8g2_t u8g2;
+
+void show_welcome(void) {
+    u8g2_Setup_st75256_i2c_jlx25664_f(&u8g2, U8G2_R0, u8x8_byte_linux_i2c, u8x8_linux_i2c_delay);
+
+    u8g2_SetI2CAddress(&u8g2, 0x3c); 
+ 
+    u8g2_InitDisplay(&u8g2);
+    u8g2_SetPowerSave(&u8g2, 0);
+	u8g2_SetContrast(&u8g2, 75);
+
+	u8g2_ClearBuffer(&u8g2);
+	u8g2_SetFont(&u8g2, u8g2_font_ncenB08_tr);
+	u8g2_DrawStr(&u8g2, 0, 24, "welcome to keyboard"); 
+	u8g2_SendBuffer(&u8g2);
+}
+
 int main() {
     const std::string broker_address = "tcp://127.0.0.1:1883";
     const std::string client_id = "paho_client";
 
     MqttMgr mqtt_mgr(broker_address, client_id);
     KeypadHandler keypad(on_keypad_msg_received);
-//FIXME: move inits out of destructor
+    
+    //FIXME: move inits out of destructor
 
     try {
         mqtt_mgr.set_message_callback(on_mqtt_msg_received);
@@ -37,14 +58,14 @@ int main() {
         std::cerr << "Error: " << e.what() << std::endl;
     }
 
-        
-        keypad.start();
+    keypad.start();
 
-        // unsigned char data_to_send[2] = { 0xA5, 0x5A };
-        // keypad.send(data_to_send);
+    // unsigned char data_to_send[2] = { 0xA5, 0x5A };
+    // keypad.send(data_to_send);
 
-        //std::this_thread::sleep_for(std::chrono::seconds(10));
-        //keypad.stop();
+    //std::this_thread::sleep_for(std::chrono::seconds(10));
+    //keypad.stop();
+    show_welcome();
 
 
     while(1)
